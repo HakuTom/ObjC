3107fe7c37bc665963cce6f0f049fd9b6eb84973
HakuTom <tomasromeo1998@gmail.com>
2016-05-31 23:09:09 +0300

    Update Cour.cs

--- Cour.cs
+++ Cour.cs
@@ -1,16 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace ConsoleApplication10
+namespace AstronomerGuide
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
-            //блабла
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
